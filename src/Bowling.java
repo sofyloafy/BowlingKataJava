df2a77229aad349ba961880339f8615150a9b2ae
Sophie <sophiebrown438@gmail.com>
2020-09-24 17:11:39 +0100

    Pass test for all Ones

--- src/Bowling.java
+++ src/Bowling.java
@@ -1,8 +1,26 @@
 public class Bowling {
-    public void roll(int i) {
+    int[] rolls = new int[21];
+    int currentRollIndex = 0;
+
+    public void roll(int roll) {
+        rolls[currentRollIndex++] = roll;
     }
 
     public int score() {
-        return 0;
+        int score = 0;
+        int index = 0;
+        for (int i = 0; i < 10; i++){
+//            System.out.println(index);
+            score += rollIndex(index++);
+            score += rollIndex(index++);
+//            System.out.println(score);
+        }
+        return score;
+    }
+
+    private int rollIndex(int... indexes) {
+        int sum = 0;
+        for (int i = 0; i < indexes.length; i++) sum += rolls[indexes[i]];
+        return sum;
     }
 }
--- tests/BowlingGameTest.java
+++ tests/BowlingGameTest.java
@@ -31,4 +31,37 @@ public class BowlingGameTest {
         bowling.roll(0);
         assertEquals(0, bowling.score());
     }
+
+    void setRolls(int n, int pin){
+        for (int i = 0; i < n; i++){
+            bowling.roll(pin);
+        }
+    }
+
+    void set2Rolls(int n, int pin1, int pin2){
+        for (int i = 0; i < n; i++){
+            bowling.roll(pin1);
+            bowling.roll(pin2);
+        }
+    }
+
+    void setArrayRolls(int[] n) {
+        for (int i = 0; i < n.length; i++){
+            bowling.roll(n[i]);
+        }
+    }
+
+    @Test
+    public void OnesTest() {
+        setRolls(20, 1);
+        assertEquals(20, bowling.score());
+    }
+
+//    @Test
+//    public void OnesBonusTest(){
+//        setRolls(10, 1);
+//        bowling.roll(10);
+//        setRolls(8,1);
+//        assertEquals(30, bowling.score());
+//    }
 }
